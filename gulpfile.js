e6823382b1b317bf4f7aca7fc31e4b2824c5b014
Patrick Burris <patrick@briefmedia.com>
2016-12-27 21:46:51 -0600

    problem with build process

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ gulp.task('css', () => {
   return gulp.src('./src/index.pcss')
     .pipe(postcss(processors))
     .pipe(rename(path => {
+      path.basename = 'fofs';
       path.extname = '.css';
       return path;
     }))
@@ -50,3 +51,17 @@ gulp.task('css', () => {
 gulp.task('watch', ['css'], () => {
   gulp.watch('src/**/*.pcss', ['css']);
 });
+
+gulp.task('minify', () => {
+
+  return gulp.src('./src/index.pcss')
+    .pipe(postcss(processors.concat(require('cssnano'))))
+    .pipe(rename(path => {
+      path.basename = 'fofs';
+      path.extname = '.min.css';
+      return path;
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('build', ['css', 'minify']);
